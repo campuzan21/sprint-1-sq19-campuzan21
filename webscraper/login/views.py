e7afc1cce9419cbdd55bd6577765d022bbeba380
campuzan21 <45133099+campuzan21@users.noreply.github.com>
2019-02-10 16:38:21 -0800

    Update views.py

--- webscraper/login/views.py
+++ webscraper/login/views.py
@@ -4,7 +4,7 @@ import requests, time
 
 
 
-https://selfserve-db.up.edu/prd/bwckschd.p_get_crse_unsec?term_in=201903&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=CS&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_dunt_unit=&sel_dunt_code=MTHS&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a
+#https://selfserve-db.up.edu/prd/bwckschd.p_get_crse_unsec?term_in=201903&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=CS&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_dunt_unit=&sel_dunt_code=MTHS&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a
 
 
 
