769483b957cb2a5484aa5e9bcb2239092502b13e
campuzan21 <45133099+campuzan21@users.noreply.github.com>
2019-02-10 16:35:28 -0800

    Add files via upload

--- webscraper/login/params.py
+++ webscraper/login/params.py
@@ -1,26 +1,65 @@
-#term_in=201903&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=CS&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_dunt_unit=&sel_dunt_code=MTHS&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a
-paramslist = {
-              "term_in": "201901", 
-              "sel_subj": "CS",
-              "sel_day": "",
-              "sel_schd": "", 
-              "sel_insm": "", 
-              "sel_camp": "dummy", 
-              "sel_levl": "", 
-              "sel_sess": "", 
-              "sel_instr": "",
-              "sel_ptrm": "",
-              "sel_attr": "", 
-              "sel_crse:": "", 
-              "sel_title": "", 
-              "sel_to_cred": "", 
-              "sel_from_cred": "",
-              "sel_dunt_unit": "", 
-              "sel_dunt_code": "", 
-              "end_mi": "", 
-              "end_hh": "", 
-              "end_ap": "", 
-              "begin_mi": "",
-              "begin_hh": "", 
-              "begin_ap": ""
-              }
+# term_in = term_in
+	# sel_subj = sel_subj
+	# sel_day = sel_day
+	# sel_crse = sel_crse
+	# sel_title = sel_title
+	# sel_from_cred = sel_from_cred
+	# sel_to_cred = sel_to_cred
+	# sel_levl = sel_levl
+	# sel_dunt_unit = sel_dunt_unit
+	# sel_dunt_code = sel_dunt_code
+	# sel_begin_hh = sel_begin_hh
+	# sel_begin_mi = sel_begin_mi
+	# sel_begin_ap = sel_begin_ap
+	# sel_end_hh = sel_end_hh
+	# sel_end_mi = sel_end_mi
+	# sel_end_ap = sel_end_ap
+
+	
+	#term_in=201903&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=CS&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_dunt_unit=&sel_dunt_code=MTHS&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a
+#levl : 02 = Grad, 01 = Undergrad, 00 = Undeclared
+#dunt_code: MTHS / WEEK
+#term_in : 201901 = Fall 2019; 201902 = Spring 2019; 201903 = Summer 2019
+#sel_day : m,t,w,r,f,s,u
+
+def genQuery(term_in, sel_subj, sel_crse, sel_title, sel_from_cred,
+				sel_to_cred, sel_levl, sel_dunt_unit, sel_dunt_code,
+				begin_hh, begin_mi, begin_ap,
+				end_hh, end_mi, end_ap):
+	#ADD POSSIBLE LIST OF DAYS?
+	
+	myURL = 'https://selfserve-db.up.edu/prd/bwckschd.p_get_crse_unsec'
+
+
+	
+	postQuery = (('term_in=%s&' +
+	'sel_subj=dummy&'+##DUPLICATED, NECESSARY FOR PROPER FORMAT
+	'sel_day=dummy&'+##DUPLICATED, NECESSARY FOR PROPER FORMAT
+	'sel_schd=dummy&'+ ##DUPLICATED, NECESSARY FOR PROPER FORMAT
+	'sel_insm=dummy&' +##NO CHANGES
+	'sel_camp=dummy&'+ ##NO CHANGES
+	'sel_levl=dummy&'+ ##DUPLICATED, NECESSARY FOR PROPER FORMAT
+	'sel_sess=dummy&'+ ##NO CHANGES
+	'sel_instr=dummy'&+ ##DUPLICATED, NECESSARY FOR PROPER FORMAT
+	'sel_ptrm=dummy&'+  ##NO CHANGES
+	'sel_attr=dummy&'+ ## NO CHANGES
+	'sel_subj=%s&'+
+	'sel_crse=%s&'+
+	'sel_title=%s&'+
+	'sel_schd=' + chr(37) + '25&'+ ##NECESSARY FORMAT
+	'sel_from_cred=%s&'+
+	'sel_to_cred=%s&'+
+	'sel_levl=' + chr(37) + '25&'+ ##NECESSARY FORMAT
+	'sel_dunt_unit=%s&'+
+	'sel_dunt_code=%s&'+
+	'sel_instr=' + chr(37) + '25&'+ ##NECESSARY FORMAT
+	'begin_hh=%s&'+
+	'begin_mi=%s&'+
+	'begin_ap=%s&'+
+	'end_hh=%s&'+
+	'end_mi=%s&'+
+	'end_ap=%s') % (term_in, sel_subj, sel_crse, sel_title, sel_from_cred,
+				sel_to_cred, sel_levl, sel_dunt_unit, sel_dunt_code,
+				begin_hh, begin_mi, begin_ap,
+				end_hh, end_mi, end_ap))
+	print(postQuery)
--- webscraper/login/views.py
+++ webscraper/login/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests, time
 
+
+
+https://selfserve-db.up.edu/prd/bwckschd.p_get_crse_unsec?term_in=201903&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=CS&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_dunt_unit=&sel_dunt_code=MTHS&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a
+
+
+
+
+
 paramslist = {
               "term_in": "201903", 
-              "sel_subj": "CS",
+              "sel_subj": ["CS","%"],
               "sel_day": "dummy",
-              "sel_schd": "dummy", 
+              "sel_schd": ["dummy","%"],
               "sel_insm": "dummy", 
               "sel_camp": "dummy", 
-              "sel_levl": "dummy", 
+              "sel_levl": ["dummy","%"], 
               "sel_sess": "dummy", 
               "sel_instr": "dummy",
               "sel_ptrm": "dummy",
@@ -31,14 +39,15 @@ paramslist = {
 
 def getCourses(params):
 	mysession = requests.session()
-	url1 = 'https://selfserve-db.up.edu/prd/bwckgens.p_proc_term_date'
+	url1 = 'https://selfserve-db.up.edu/prd/bwckschd.p_disp_dyn_sched'
+	#https://selfserve-db.up.edu/prd/bwckgens.p_proc_term_date
 	url2 = 'https://selfserve-db.up.edu/prd/bwckschd.p_get_crse_unsec'
 	header = {'Accept-Encoding': 'gzip, deflate, sdch',
 				'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.'}
 	
-	mysession.post(url1, headers = header, params = {"p_calling_proc": "bwckschd.p_disp_dyn_sched", "p_term": "201903"})
-	time.sleep(5)
+	#return mysession.post(url1, headers = header, params = {"p_calling_proc": "bwckschd.p_disp_dyn_sched", "p_term": "201903"})
+	#time.sleep(5)
 	return mysession.post(url2, headers = header, params=paramslist)
 	
 def index(request):
